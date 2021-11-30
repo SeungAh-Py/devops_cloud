e02ca5c4297455231c49565ca090b1f123c42f15
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-01 03:16:11 +0900

    5. 각자의 모델을 1개 생성 / 마이그레이션 수행

--- myhomework11/catblog/models.py
+++ myhomework11/catblog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    cat_name = models.CharField(max_length=10)
+    place_origin = models.CharField(max_length=10)
+    cat_weight = models.CharField(max_length=11)
+    cat_info = models.TextField(verbose_name="기본정보")
+    cat_character = models.TextField(verbose_name="성격")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d/%H')
