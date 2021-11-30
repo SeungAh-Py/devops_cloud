0db80b427d74b58a35f0fd386315716a76c75d14
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-01 03:30:15 +0900

    6. admin에 등록

--- myhomework11/catblog/admin.py
+++ myhomework11/catblog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from catblog.models import Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["cat_name", "cat_weight", "cat_healthy", "created_at"]
+    search_fields = ["cat_name", "cat_character"]
+
+
 admin.site.register(Post)
