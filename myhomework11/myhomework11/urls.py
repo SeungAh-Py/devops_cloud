c381f09c73bf5dcd27722531e4576a469cd49717
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-01 01:51:40 +0900

    3. MEDIA 관련 설정

--- myhomework11/myhomework11/urls.py
+++ myhomework11/myhomework11/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# MEDIA 관련 추가 이하 2개
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# MEDIA 추가
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
