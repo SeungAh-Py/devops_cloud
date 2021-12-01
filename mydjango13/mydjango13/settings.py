9ebf96847d71e310abf8e12024f33a8fcb29a2a8
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-01 18:00:31 +0900

    개발모드에서 SQL 화면출력 로깅 활성화

--- mydjango13/mydjango13/settings.py
+++ mydjango13/mydjango13/settings.py
@@ -139,9 +139,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 INTERNAL_IPS = ['127.0.0.1']
+
 
 if DEBUG:
     LOGGING = {
