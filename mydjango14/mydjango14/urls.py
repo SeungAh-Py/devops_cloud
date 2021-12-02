3dd6d7fe29c97f09fb122d1fffb9cb6cf64f7351
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-02 17:31:30 +0900

    catube/index 뷰 구현

--- mydjango14/mydjango14/urls.py
+++ mydjango14/mydjango14/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from catube.views import index, video_datail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("catube/", index),
+    path("catube/<int:pk>/", video_datail),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -12,6 +15,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
-            path('__debug__/', include(debug_toolbar.urls)),
-        ]
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
