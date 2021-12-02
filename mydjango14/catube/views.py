cb5057c3a3003658d1b4b5ffe9d18c3ec2172410
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-02 17:27:12 +0900

    catube 앱 생성 및 등록

---
+++ mydjango14/catube/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from catube.models import Video
+
+# Class Based View (CBV)
+# index = ListView.as_view(model=Video, template_name="catube/index.html")
+
+
+# Function Based View (FBV)
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Video.objects.all()
+    return render(
+        request,
+        "catube/index.html",
+        {
+            "video_list": qs,
+        },
+    )
+
+
+def video_datail(request: HttpRequest, pk: int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(
+        request,
+        "catube/video_detail.html",
+        {
+            "video": video,
+        },
+    )
