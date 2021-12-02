5747a6b8c34385f103d388b0bc0102b9e7499e9f
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-02 20:36:48 +0900

    중간 수정

--- myhomework14/myhomework14/urls.py
+++ myhomework14/myhomework14/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from favoritube.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favoritube/', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
