a0f12a5e8de31c910e6b132cffc4edfc0c696cc4
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-15 06:30:10 +0900

    Model (Shop,Category,Tag,Review) 생성 및 등록, 마이그레이션

--- myhomework21_1/shop/admin.py
+++ myhomework21_1/shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Shop, Category, Tag, Review
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["author_name"]
