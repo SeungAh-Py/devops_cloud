bd11e317ac9de21ca02c51fa3d3b45bc224be20e
SeungAh Kim <cpuasdnwin@naver.com>
2021-12-24 02:00:25 +0900

    git added myreact29

---
+++ myreact29/src/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Counter () {
+  const [value, setValue] = useState(10);
+  const [color, setColor] = useState("red");
+
+  const handleClick = () => {
+    console.log(`clicked`);
+    setValue(value + 1);
+    setColor( value % 2 === 0 ? "green" : "red");
+  };
+  return (
+    <div style={{ 
+      backgroundColor: color,
+      width: 100,
+      height: 100,
+      display: "inline-block",
+      margin: 5,
+      borderRadius: 50,
+      textAlign: "center",
+      lineHeight: "5.5",
+      fontSize: 17,
+      fontWeight: "bold",
+      color: "white",
+      userSelect: "none",
+      cursor: 'pointer',
+     }}
+     onClick={handleClick}
+     >      
+      <span>{value}</span>
+    </div>
+  );
+}
+
+export default Counter;
