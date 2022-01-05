24ea1f50466321ea3d7a06effef23dc5d2d42b9d
SeungAh Kim <cpuasdnwin@naver.com>
2022-01-06 06:52:36 +0900

    Todo delete 기능 구현

--- mystudy37/src/components/TodoList.jsx
+++ mystudy37/src/components/TodoList.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import Todo from './Todo';
 import TodoForm from './TodoForm';
 
+const INITIAL_STATE = [
+  { content: 'Python' },
+  { content: 'Django' },
+  { content: 'React' },
+];
+
 function TodoList() {
+  const [todoList, setTodoList] = useState(INITIAL_STATE);
+
+  const deleteHandler = (todoIndex) => {
+    setTodoList(todoList.filter((_, index) => todoIndex !== index));
+  };
+
   return (
     <div>
       <TodoForm />
-      <Todo />
+      {todoList.map((todo, index) => (
+        <Todo todo={todo} onClick={() => deleteHandler(index)} />
+      ))}
     </div>
   );
 }
